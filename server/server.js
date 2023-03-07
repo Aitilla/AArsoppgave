4601bf44b3d14c6ee87a112e7486296e456f6462
Aitilla <jhertaas@gmail.com>
2023-03-07 11:54:55 +0100

    loggingh

--- server/server.js
+++ server/server.js
@@ -4,6 +4,11 @@ const path = require('path');
 const app = express();
 const PORT = 8080;
 
+app.use('/', (req, res, next) => {
+    console.log(`noen som kobla seg til`);
+    next();
+})
+
 app.use('/', express.static(path.join('..', 'website')));
 
 app.listen(PORT, () => {
