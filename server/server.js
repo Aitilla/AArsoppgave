37ada67af082ef820c00211f39a21dd884543266
Aitilla <jhertaas@gmail.com>
2023-03-07 11:51:00 +0100

    jlaskdøjgla

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 8080;
+const PORT = 80;
 
 app.use('/', express.static(path.join('..', 'website')));
 
